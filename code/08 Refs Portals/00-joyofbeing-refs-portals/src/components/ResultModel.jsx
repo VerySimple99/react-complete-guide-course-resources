22184e7c2a3fe477f38ae686d2697b2c1cdd3174
VerySimple99 <lyricrain.seo@gmail.com>
2025-03-27 18:48:22 +0900

    feat: stillness refs

---
+++ code/08 Refs Portals/00-joyofbeing-refs-portals/src/components/ResultModel.jsx
@@ -0,0 +1,23 @@
+import { useImperativeHandle, useRef } from "react";
+
+export default function ResultModal({ref, result, targetTime}){
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    };
+  });
+
+  return (
+    <dialog ref={dialog} className="result-modal" open>
+      <h2>Your {result}</h2>
+      <p>The target time was <strong>{targetTime} second.</strong> </p>
+      <p>You stopped the timer with <strong>X seconds left.</strong></p><form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+}
